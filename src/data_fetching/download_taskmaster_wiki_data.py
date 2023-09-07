68d6a0eaee605088a521789b9ff243aa6d5badac
cbrochtrup <cbrochtrup@greenkeytech.com>
2023-09-06 20:12:40 -0500

    Parse many URLs at once. Process values for tie breaker tasks and disqualified participants.

--- src/data_fetching/download_taskmaster_wiki_data.py
+++ src/data_fetching/download_taskmaster_wiki_data.py
@@ -1,23 +1,62 @@
-import sys
+import os
 
 import pandas as pd
 
 """
-Example usage:
-    src/download_taskmaster_wiki_data.py https://taskmaster.fandom.com/wiki/Series_1
+Download data from the urls below and parse into a format ready
+to load into our database
 
-This will create a CSV file for each episode on the page.
+This script create a folder for each series
+containing a CSV file for each episode
 """
 
-wiki_url_to_parse = sys.argv[1]
-table = pd.read_html(wiki_url_to_parse, attrs={"class": "tmtable"})
-df = table[0]
+series_urls = (
+    "https://taskmaster.fandom.com/wiki/Series_1",
+    "https://taskmaster.fandom.com/wiki/Series_2",
+    "https://taskmaster.fandom.com/wiki/Series_3",
+    "https://taskmaster.fandom.com/wiki/Series_4",
+    "https://taskmaster.fandom.com/wiki/Series_5",
+    "https://taskmaster.fandom.com/wiki/Champion_of_Champions",
+    "https://taskmaster.fandom.com/wiki/Series_6",
+    "https://taskmaster.fandom.com/wiki/Series_7",
+    "https://taskmaster.fandom.com/wiki/Series_8",
+    "https://taskmaster.fandom.com/wiki/Series_9",
+    "https://taskmaster.fandom.com/wiki/Series_10",
+    "https://taskmaster.fandom.com/wiki/Series_11",
+    "https://taskmaster.fandom.com/wiki/Series_12",
+    "https://taskmaster.fandom.com/wiki/Series_13",
+    "https://taskmaster.fandom.com/wiki/Series_14",
+    "https://taskmaster.fandom.com/wiki/Series_15",
+    "https://taskmaster.fandom.com/wiki/Series_16",
+    "https://taskmaster.fandom.com/wiki/Champion_of_Champions_II",
+)
+# TODO: parse these in the future. They're not very interesting anyway
+ny_urls = (
+    # These aren't organized the same as the series
+    "https://taskmaster.fandom.com/wiki/Taskmaster%27s_New_Year_Treat#2022",
+    "https://taskmaster.fandom.com/wiki/Taskmaster%27s_New_Year_Treat#2021",
+    "https://taskmaster.fandom.com/wiki/Taskmaster%27s_New_Year_Treat#2023",
+)
 
-inds_table = df[df["Task"].str.contains("Episode")].index
-total_inds = inds_table.to_list() + [len(df) -1]
+for wiki_url_to_parse in series_urls:
+    page_name = wiki_url_to_parse.split("/")[-1]
+    output_dir = os.path.join("data", page_name)
+    os.makedirs(output_dir, exist_ok=True)
 
-for ind1, ind2 in zip(total_inds, total_inds[1:]):
-    episode_name = df.iloc[ind1, 0]
-    # ind2 is the row for the Total and we can calculate that ourselves
-    df_episode = df.iloc[ind1+1: ind2-1]
-    df_episode.to_csv(f"{episode_name}.csv", index=False)
+    table = pd.read_html(wiki_url_to_parse, attrs={"class": "tmtable"})
+    df = table[0]
+
+    inds_table = df[df["Task"].str.contains("Episode")].index
+    total_inds = inds_table.to_list() + [len(df) -1]
+
+    for ind1, ind2 in zip(total_inds, total_inds[1:]):
+        episode_name = df.iloc[ind1, 0]
+        # ind2 is the row for the Total and we can calculate that ourselves
+        df_episode = df.iloc[ind1+1: ind2-1]
+
+        # Set disqualified competitors to null
+        df_episode = df_episode.replace("DQ", "")
+
+        tie_breaker_row = df_episode["Task"] == "T"
+        df_episode[tie_breaker_row] = df_episode[tie_breaker_row].replace({"✔": 1, '✘': 0, "–": ""})
+        df_episode.to_csv(f"{output_dir}/{episode_name}.csv", index=False)
