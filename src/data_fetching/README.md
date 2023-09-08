839a7695d6c30e0c6f33b02dde8ab15b3a919489
Colin Brochtrup <brochtrup.colin@gmail.com>
2023-09-08 10:42:31 -0500

    Parse taskmaster wiki examples into DB friendly format (#6)
    * Add an example script to parse data from the tables on the taskmaster wiki. This would be an easy way to get data for all of the episodes. Add a small script with plotly usage, a solid open source library for visualizations.
    * Add comment describing usage.
    * Add requirements.txt file.
    * Reorganize data fetching script and remove visualization script.
    * Parse many URLs at once. Process values for tie breaker tasks and disqualified participants.
    * Comment URL for taskmaster series that has not aired yet.
    * Remove unnecessary items fro gitignore.
    * Add short README.md with setup instructions.

---
+++ src/data_fetching/README.md
@@ -0,0 +1,20 @@
+# Data Fetching
+
+This directory houses our scripts to fetch and preprocess taskmaster wiki data.
+
+## Running The Script
+
+
+### Setup Environment
+
+```bash
+python3.8 -m venv venv
+source venv/bin/activate
+pip install wheel
+pip install -r requirements.txt
+```
+### Execute Script
+
+```bash
+python download_taskmaster_wiki_data.py
+```
---
+++ src/data_fetching/download_taskmaster_wiki_data.py
@@ -0,0 +1,63 @@
+import os
+
+import pandas as pd
+
+"""
+Download data from the urls below and parse into a format ready
+to load into our database
+
+This script create a folder for each series
+containing a CSV file for each episode
+"""
+
+series_urls = (
+    "https://taskmaster.fandom.com/wiki/Series_1",
+    "https://taskmaster.fandom.com/wiki/Series_2",
+    "https://taskmaster.fandom.com/wiki/Series_3",
+    "https://taskmaster.fandom.com/wiki/Series_4",
+    "https://taskmaster.fandom.com/wiki/Series_5",
+    "https://taskmaster.fandom.com/wiki/Champion_of_Champions",
+    "https://taskmaster.fandom.com/wiki/Series_6",
+    "https://taskmaster.fandom.com/wiki/Series_7",
+    "https://taskmaster.fandom.com/wiki/Series_8",
+    "https://taskmaster.fandom.com/wiki/Series_9",
+    "https://taskmaster.fandom.com/wiki/Series_10",
+    "https://taskmaster.fandom.com/wiki/Series_11",
+    "https://taskmaster.fandom.com/wiki/Series_12",
+    "https://taskmaster.fandom.com/wiki/Series_13",
+    "https://taskmaster.fandom.com/wiki/Series_14",
+    "https://taskmaster.fandom.com/wiki/Series_15",
+    "https://taskmaster.fandom.com/wiki/Champion_of_Champions_II",
+    # This series airs on September 21st
+    # "https://taskmaster.fandom.com/wiki/Series_16",
+)
+# TODO: parse these in the future. They're not very interesting anyway
+ny_urls = (
+    # These aren't organized the same as the series
+    "https://taskmaster.fandom.com/wiki/Taskmaster%27s_New_Year_Treat#2022",
+    "https://taskmaster.fandom.com/wiki/Taskmaster%27s_New_Year_Treat#2021",
+    "https://taskmaster.fandom.com/wiki/Taskmaster%27s_New_Year_Treat#2023",
+)
+
+for wiki_url_to_parse in series_urls:
+    page_name = wiki_url_to_parse.split("/")[-1]
+    output_dir = os.path.join("data", page_name)
+    os.makedirs(output_dir, exist_ok=True)
+
+    table = pd.read_html(wiki_url_to_parse, attrs={"class": "tmtable"})
+    df = table[0]
+
+    inds_table = df[df["Task"].str.contains("Episode")].index
+    total_inds = inds_table.to_list() + [len(df) -1]
+
+    for ind1, ind2 in zip(total_inds, total_inds[1:]):
+        episode_name = df.iloc[ind1, 0]
+        # ind2 is the row for the Total and we can calculate that ourselves
+        df_episode = df.iloc[ind1+1: ind2-1]
+
+        # Set disqualified competitors to null
+        df_episode = df_episode.replace("DQ", "")
+
+        tie_breaker_row = df_episode["Task"] == "T"
+        df_episode[tie_breaker_row] = df_episode[tie_breaker_row].replace({"✔": 1, '✘': 0, "–": ""})
+        df_episode.to_csv(f"{output_dir}/{episode_name}.csv", index=False)
